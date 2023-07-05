39295e809fb7e2b84cb2dc107f2b96d2676e4f88
刘天宇 <liutianyu1250@phytium.com.cn>
2023-07-05 01:52:23 +0000

    Update to kernel_5.10-rt_2023-v1.0-RC1

---
+++ drivers/crypto/te/hosal/src/osal/envs/linux_kernel/osal_log.c
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2020 ARM Technology (China) Co., Ltd.
+ */
+#include <linux/kernel.h>
+#include <linux/printk.h>
+#include "osal_log.h"
+#include <stdarg.h>
+
+__attribute__((format(printf, 1, 2))) int32_t osal_log_printf(const char *fmt,
+                                                              ...)
+{
+    va_list args;
+
+    va_start(args, fmt);
+    vprintk(fmt, args);
+    va_end(args);
+
+    return 0;
+}
