74bc4adc08e72adf0c2b57fd9abbdbdbd0552435
YuDa <yuda2152@phytium.com.cn>
2024-06-12 03:25:41 +0000

    drm/phytium: Add PWM backlight control for pe220x eDP
    When the pe220x is connected with eDP,
    the backlight of eDP screen is controlled
    using PWM and GPIO modules
    
    Signed-off-by: YuDa <yuda2152@phytium.com.cn>
    Change-Id: Ia60c14d3081539f522a6b8e7528446754ef12f1b

--- drivers/gpu/drm/phytium/phytium_display_drv.h
+++ drivers/gpu/drm/phytium/phytium_display_drv.h
@@ -8,6 +8,7 @@
 #define __PHYTIUM_DISPLAY_DRV_H__
 
 #include <drm/drm_print.h>
+#include <linux/pwm.h>
 #include <drm/drm_fb_helper.h>
 
 #define DEBUG_LOG 0
@@ -69,7 +70,9 @@ struct phytium_device_info {
 	unsigned int hdisplay_max;
 	unsigned int vdisplay_max;
 	unsigned int backlight_max;
+	unsigned int backlight_min;
 	unsigned long address_mask;
+	struct pwm_device *pwm;
 };
 
 struct phytium_display_private {
@@ -115,6 +118,8 @@ struct phytium_display_private {
 	/* DMA info */
 	int dma_inited;
 	struct dma_chan *dma_chan;
+	/*BL GPIO info*/
+	struct gpio_desc *edp_bl_en, *edp_power_en;
 };
 
 static inline unsigned int
